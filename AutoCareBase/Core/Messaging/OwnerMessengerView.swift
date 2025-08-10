6f4e61dd7cbf9f04747e6675a1841f5a501944bd
luis-f-santos <luissantos@Luiss-iMac.lan>
2025-08-10 03:35:37 -0500

    Adding initial Messaing file

---
+++ AutoCareBase/Core/Messaging/OwnerMessengerView.swift
@@ -0,0 +1,20 @@
+//
+//  OwnerMessengerView.swift
+//  AutoCareBase
+//
+//  Created by Luis Santos on 8/7/25.
+//
+
+import SwiftUI
+
+struct OwnerMessengerView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OwnerMessengerView()
+}
